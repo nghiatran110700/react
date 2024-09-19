7b12c7eed6fcbe558b7d258a256c129fb911361b
ttnghia3 <ttnghia3@cmc.com.vn>
2024-09-19 09:31:24 +0700

    init project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import ShoppingList from './component/ShopList';
+import Clock from './component/Clock';
+import React, { useState, useEffect } from 'react';
+import Modal from './component/Modal/Modal'
+import useModal from './component/Modal/useModal';
+
+function MyButton() {
+  function handleClick(){
+      console.log('test click')
+  }
+
+  return (
+    <button onClick={handleClick}>I'm a button</button>
+  );
+}
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(new Date());
+  const {isShowing, toggle} = useModal();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+    
+    return () => clearInterval(interval); 
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +35,14 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button className="button-default" onClick={toggle}>Show Modal</button>
+      <Modal
+        isShowing={isShowing}
+        hide={toggle}
+      />
+        <MyButton/>
+        <ShoppingList/>
+        <Clock time={currentTime}/>
       </header>
     </div>
   );
---
+++ src/component/Clock.js
@@ -0,0 +1,9 @@
+export default function Clock({ time }) {
+    return (
+      <>
+        <h1>{time.toLocaleTimeString()}</h1>
+        <input />
+      </>
+    );
+  }
+  
